edb6b5a7c9502aef7f7473dbd6bbd4a27bb64588
koutalou <k.koutalou@gmail.com>
2017-10-25 03:20:15 +0900

    :wrench: move enum define to module.exports

--- eventhandler.js
+++ eventhandler.js
@@ -1,10 +1,11 @@
 const RNEventHandler = require('react-native').NativeModules.RNEventHandler;
-const RNEventType = {
-    nativeEventTypeUserDidTakeScreenshot: 1,
-};
 
 module.exports = {
     addListener: function (callback) {
 	return RNEventHandler.watch(callback);
+    },
+
+    const RNEventType = {
+	nativeEventTypeUserDidTakeScreenshot: 1,
     },
 };
