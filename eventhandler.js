ee4edfd1b398235cfbe7f62daef719f7106885c5
koutalou <k.koutalou@gmail.com>
2017-10-25 03:01:08 +0900

    :wrench: set fixed value name for event type such like enum

--- eventhandler.js
+++ eventhandler.js
@@ -1,4 +1,7 @@
 const RNEventHandler = require('react-native').NativeModules.RNEventHandler;
+const RNEventType = {
+    nativeEventTypeUserDidTakeScreenshot: 1,
+};
 
 module.exports = {
     addListener: function (callback) {
